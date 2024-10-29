9087d2e4fdee2e8ba233eb46c54646a85c23963a
Robert Rosati <robbie@davinci.nsstc.nasa.gov>
2024-10-29 12:42:45 -0500

    more runs

--- test_runs.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-rm -rf ./chains
-rm -rf ./data
-OMP_NUM_THREADS=24 build/apps/src/noise_mcmc --sim-noise --conf-noise --steps 2000 --chains 24 --cheat --fmin 1e-4 --fmax 1e-2 --duration 1966080
-
